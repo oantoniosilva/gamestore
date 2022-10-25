93928ea5cf0eec5bf8873cd293ee1315d5a3055d
Antonio <antoniopsilva083@gmail.com>
2022-10-25 01:26:56 -0300

    feat: create typography component and typography style

---
+++ src/styles/typography.ts
@@ -0,0 +1,57 @@
+import styled, { css } from "styled-components";
+import { BaseTitle } from "./components/typography";
+
+export const StyledTitle = styled(BaseTitle)`
+  font-family: 'Poppins', sans-serif;
+  font-weight: ${({ fontWeight }) => fontWeight};
+  ${({ fontSize }) => {
+    switch(fontSize) {
+      case 'lg':
+        return css`
+          font-size: 44px;
+        `
+      case 'md':
+        return css`
+          font-size: 20px;
+        `
+      case 'sm':
+        return css`
+          font-size: 16px;
+        `
+    }
+  }}
+`;
+
+interface IStyledParagraph {
+  opacity?: number;
+  fontSize: 'lg' | 'md' | 'sm';
+}
+
+export const StyledParagraph = styled.p<IStyledParagraph>`
+  font-family: 'Poppins', sans-serif;
+  
+  // toda tag strong ou b(bold) que estiver dentro da tag p será negrito
+  strong, b {
+    font-weight: 700;
+  }
+                                            
+  opacity: ${({ opacity }) => opacity ? opacity : .5}; // .5 = 50% de opacidade.
+
+  ${({ fontSize }) => {
+    switch(fontSize) {
+      case 'lg':
+        return css`
+          font-size: 20px;
+        `
+      case 'md':
+        return css`
+          font-size: 16px;
+        `
+      case 'sm':
+        return css`
+          font-size: 14px;
+        `
+    }
+  }}
+  
+`;
